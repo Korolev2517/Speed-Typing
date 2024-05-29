14d25bb4bf9a950c840cd2d81c7481c2fab44bfc
Konstantin <60309111+Korolev2517@users.noreply.github.com>
2024-05-29 22:23:55 +0700

    error count

--- main.js
+++ main.js
@@ -1,31 +1,10 @@
 const FISH_TEXT = 'https://fish-text.ru/get?';
 const textContent = document.querySelector('.text-content');
 const inputText = document.querySelector('.input-text');
+const time = document.querySelector('.time');
+const error = document.querySelector('.error');
 
-
-inputText.addEventListener('input', () => {
-  let correct = true; // переменная для проверки, все ли символы в поле ввода совпадают с текстом в textContent
-  const textContentArray = textContent.querySelectorAll('span');
-  const inputTextArray = inputText.value.split('');
-  textContentArray.forEach((element, index) => {
-    let newElement = inputTextArray[index]; // в переменную помещаем символ из массива inputTextArray, который соответвует текущему индексу в textContentArray
-    console.log(newElement)
-    if (newElement == null) { // проверяем сущесвтует ли символ в inputTextArray для текущего индекса
-      element.classList.remove('correct');
-      element.classList.remove('incorrect');
-      correct = false;
-    }
-    else if (newElement === element.innerText) {
-      element.classList.add('correct');
-      element.classList.remove('incorrect');
-    } else {
-      element.classList.remove('correct');
-      element.classList.add('incorrect');
-      correct = false;
-    }
-  })
-  if (correct) getNextRandomText()
-})
+let previousInput = 0; // переменная в которую будет помещаться предыдущая длинна ввода
 
 function getRandomText() {
   const type = 'sentence';
@@ -47,5 +26,37 @@ async function getNextRandomText() {
     textContent.appendChild(elementSpan);
   })
   inputText.value = null;
+  previousInput = 0; // сброс предъыдущей длинны ввода при получении нового текста
 }
-getNextRandomText()
+getNextRandomText();
+
+inputText.addEventListener('input', () => {
+  let correct = true; // переменная для проверки, все ли символы в поле ввода совпадают с текстом в textContent
+  let incorrect = false; // переменная для проверки ввода неправильнх символов
+  const textContentArray = textContent.querySelectorAll('span');
+  const inputTextArray = inputText.value.split('');
+  textContentArray.forEach((element, index) => {
+    let newElement = inputTextArray[index]; // в переменную помещаем символ из массива inputTextArray, который соответвует текущему индексу в textContentArray
+    if (newElement == null) { // проверяем сущесвтует ли символ в inputTextArray для текущего индекса
+      element.classList.remove('correct');
+      element.classList.remove('incorrect');
+      correct = false;
+    }
+    else if (newElement === element.innerText) {
+      element.classList.add('correct');
+      element.classList.remove('incorrect');
+    } else {
+      element.classList.remove('correct');
+      element.classList.add('incorrect');
+      correct = false;
+      incorrect = true; // помечаем что был введен неправильный символ
+    }
+  })
+  if (incorrect && inputText.value.length > previousInput) { // если был введен неправильный символ, то увеличиваем счетчик ошибок
+    error.innerHTML++;
+  }
+  previousInput = inputText.value.length; // обновление значения предыдущей длинны ввода
+  if (textContentArray.length == inputTextArray.length) getNextRandomText()
+})
+
+
